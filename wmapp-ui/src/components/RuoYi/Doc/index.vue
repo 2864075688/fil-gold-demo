1a8c58432a9298c91e5454b571fd64473bdcc265
2864075688 <95845993+2864075688@users.noreply.github.com>
2021-12-10 09:11:46 +0800

    Add files via upload

---
+++ wmapp-ui/src/components/RuoYi/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="question" @click="goto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'doc',
+  data() {
+    return {
+      url: ''
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
